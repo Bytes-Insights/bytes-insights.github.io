3dadbe5903584d17a749730a0f9132abf96a689e
tiagombp <tiagombp@gmail.com>
2023-09-24 23:48:40 +0200

    fixes position calculation

--- script.js
+++ script.js
@@ -3,23 +3,25 @@ const header = document.querySelector('header');
 
 header.addEventListener('mousemove', mouseMove);
 
-const h = +window.getComputedStyle(shadow).height.slice(0,-2);
-const w = +window.getComputedStyle(shadow).width.slice(0,-2);
+const shadowBbox = shadow.getBoundingClientRect();
+
+const { x, y, width, height } = shadowBbox;
+
+console.log(x, y, width, height);
+
+//const h = +window.getComputedStyle(shadow).height.slice(0,-2);
+//const w = +window.getComputedStyle(shadow).width.slice(0,-2);
 
 function mouseMove(e) {
 
-    
+    const mx = e.clientX;
+    const my = e.clientY;
 
-    const x = e.screenX;
-    const y = e.screenY;
-
-    const dx = Math.round(100 * x / w);
-    const dy = Math.round(100 * y / h);
+    const dx = Math.round(100 * (mx - x) / width);
+    const dy = Math.round(100 * (my - y) / height);
 
     //shadow.style.perspectiveOrigin = `${dx}% ${dy}%`;
     shadow.style.setProperty('--x', `${dx}%`);
     shadow.style.setProperty('--y', `${dy}%`);
-
-    console.log(dx, dy);
 
 }
