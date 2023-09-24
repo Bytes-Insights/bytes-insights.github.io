2774d32a4955bdc2276c43bbd080f8f9d19a45f4
tiagombp <tiagombp@gmail.com>
2023-09-24 17:08:55 +0200

    first experiment

---
+++ script.js
@@ -0,0 +1,21 @@
+const shadow = document.querySelector('.threeD-shadow');
+const header = document.querySelector('header');
+
+header.addEventListener('mousemove', mouseMove);
+
+const h = +window.getComputedStyle(header).height.slice(0,-2);
+const w = +window.getComputedStyle(header).width.slice(0,-2);
+
+function mouseMove(e) {
+
+    console.log(e);
+
+    const x = e.screenX;
+    const y = e.screenY;
+
+    const dx = Math.round(100 * x / w);
+    const dy = Math.round(100 * y / h);
+
+    shadow.style.perspectiveOrigin = `${dx}% ${dy}%`;
+
+}
